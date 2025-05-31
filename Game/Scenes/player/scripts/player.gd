01fe2026efcb68908433e8452bc51c29ac163ba8
Rui Ribeiro <42305006+ruiribeiro04@users.noreply.github.com>
2025-05-31 07:13:38 +0100

    Implemented a Player State Manager/Machine, created a playground scene to test progress

--- Game/Scenes/player/scripts/player.gd
+++ Game/Scenes/player/scripts/player.gd
@@ -2,24 +2,23 @@ class_name Player extends CharacterBody2D
 
 var cardinal_direction: Vector2 = Vector2.DOWN
 var direction : Vector2 = Vector2.ZERO
-var move_speed : float = 250.0
-var state : String = "idle"
 
 # For what I could find online, we need this to access the variables through code
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var state_machine: PlayerStateMachine = $StateMachine
+
 
 func _ready() -> void:
-	pass
+	state_machine.Initialize(self)
+
 
 # The "_process" function it's called every frame, so we need to be careful to be the more optimized as possible
 func _process(delta: float) -> void:
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	
-	velocity = direction * move_speed
-	if SetState() || SetDirection():
-		UpdateAnimation()
+
 
 # In the other hand, this physics "thing" runs in a fixed rate, so, by using this we can be more optimized, at least.. in theory.
 func _physics_process(delta: float) -> void:
@@ -41,18 +40,13 @@ func SetDirection() -> bool:
 		return false
 	cardinal_direction = new_direction
 	return true
-	
-func SetState() -> bool:
-	var new_state : String = "idle" if direction == Vector2.ZERO else "walk"
-	if new_state == state:
-		return false
-	state = new_state
-	return true
-	
-func UpdateAnimation() -> void:
+
+
+func UpdateAnimation( state : String ) -> void:
 	animation_player.play(state + "_" + AnimDirection())
 	pass
-	
+
+
 func AnimDirection() -> String:
 	if cardinal_direction == Vector2.DOWN:
 		return "down"
