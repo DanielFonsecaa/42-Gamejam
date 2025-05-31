c10cfbbfb6e626fdd572285e31c31ce695d86945
Danielfonsecaa <danieldafonseca@sapo.pt>
2025-05-31 10:52:08 +0100

    add clients and maybe test player

---
+++ Game/Scenes/client/character_body_2d.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	# if not is_on_floor():
+	#	velocity += get_gravity() * delta
+
+	# Handle jump.
+	var vertical := Input.get_axis("ui_up", "ui_down")
+	if vertical:
+		velocity.y = vertical * SPEED
+	else:
+		velocity.y = move_toward(velocity.x, 0, SPEED)
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var horizotal := Input.get_axis("ui_left", "ui_right")
+	if horizotal:
+		velocity.x = horizotal * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
