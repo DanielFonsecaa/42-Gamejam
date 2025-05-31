51cbb2e4d26c5111d48bb1af464a5c5901821231
Danielfonsecaa <danieldafonseca@sapo.pt>
2025-05-31 20:22:28 +0100

    add sickeness ballons and animations

--- Game/Scenes/enemy/enemy.gd
+++ Game/Scenes/enemy/enemy.gd
@@ -11,6 +11,13 @@ var client_scenes = [
 	preload("res://Scenes/client/client_queen.tscn"),
 	preload("res://Scenes/client/client_worker.tscn"),
 ]
+var sicknesses = [
+	preload("res://Scenes/sickness/fever.tscn"),
+	preload("res://Scenes/sickness/poison.tscn"),
+	preload("res://Scenes/sickness/injure.tscn"),
+]
+
+var healthy_animation = preload("res://Scenes/sickness/healthy.tscn")
 
 func _process(delta: float) -> void:
 	pass
@@ -28,10 +35,20 @@ func spawn_client() -> void:
 			bed.occupy()
 			var rand_index = randi() % client_scenes.size()
 			var client_instance = client_scenes[rand_index].instantiate()
-			client_instance.position = Vector2(70, 400)  # spawn start pos
-			print("Assigning bed_position:", bed.global_position)
+			client_instance.position = Vector2(70, 400)
 			client_instance.bed_position = bed.global_position
 			client_instance.assigned_bed = bed
+
+			# Add client first
 			get_tree().current_scene.add_child(client_instance)
+
+			# Add sickness icon as a child of client
+			var rand_sick = randi() % sicknesses.size()
+			var sickness_icon_instance = sicknesses[rand_sick].instantiate()
+			client_instance.sickness_name = sickness_icon_instance.name
+			# Position relative to client origin (local position)
+			sickness_icon_instance.position = Vector2(-55, -20)  # adjust offset as you want
+			client_instance.add_child(sickness_icon_instance)
+
 			return
-	print("❌ No available beds, not spawning new client.")
+	print("❌ No available beds, not spawning new client.")                             
